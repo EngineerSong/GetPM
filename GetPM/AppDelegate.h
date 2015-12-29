672e34ea18259642e154bc74023b5bcc00e1033f
barara <barara@bararadeimac.lan>
2015-12-29 17:16:51 +0800

    Initial Commit

---
+++ GetPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetPM
+//
+//  Created by barara on 15/12/29.
+//  Copyright © 2015年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetPM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GetPM
+//
+//  Created by barara on 15/12/29.
+//  Copyright © 2015年 Jay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
